ce576f2ed9cae3100706614daa58f2707009c78a
Tommy Zhang <zwt03712@gmail.com>
2024-04-30 02:00:04 +1000

    sortable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,55 @@
 "use client";
-import { Joker } from "@/components/Joker";
+
+import { useRef, useState } from "react";
+import Tilt from "react-parallax-tilt";
 
 export default function Home() {
-  const jokers = [
-    "Canio.webp",
-    "Chicot.webp",
-    "Perkeo.webp",
-    "Triboulet.webp",
-    "Yorick.webp",
-    "Joker.webp",
-    "Smeared_Joker.webp",
-    "Baron.webp",
-    "Baseball_Card.webp",
-    "Sixth_Sense.webp",
-    "The_Duo.webp",
-    "The_Family.webp",
-    "The_Order.webp",
-    "The_Tribe.webp",
-    "The_Trio.webp",
-  ];
+  const [jokers, setJokers] = useState([
+    { id: 0, name: "Canio" },
+    { id: 1, name: "Chicot" },
+    { id: 2, name: "Perkeo" },
+    { id: 3, name: "Triboulet" },
+    { id: 4, name: "Yorick" },
+    { id: 5, name: "Joker" },
+    { id: 6, name: "Smeared_Joker" },
+    { id: 7, name: "Baron" },
+    { id: 8, name: "Baseball_Card" },
+    { id: 9, name: "Sixth_Sense" },
+    { id: 10, name: "The_Duo" },
+    { id: 11, name: "The_Trio" },
+    { id: 12, name: "The_Family" },
+    { id: 13, name: "The_Order" },
+    { id: 14, name: "The_Tribe" },
+  ]);
+
+  const dragJoker = useRef<number>(0);
+  const draggedOverJoker = useRef<number>(0);
+
+  const handleSort = () => {
+    const newJokers = [...jokers];
+    const temp = newJokers[dragJoker.current];
+    newJokers[dragJoker.current] = newJokers[draggedOverJoker.current];
+    newJokers[draggedOverJoker.current] = temp;
+    setJokers(newJokers);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-600">
       <section className="bg-[#394b4d] p-8 rounded-2xl border-4 border-[#b1c6c9] shadow-[0_6px_0_#777383] flex flex-col justify-center items-center">
         <div className="wrapper grid grid-cols-5 pb-2 -mx-2 gap-2 rounded-xl bg-[rgb(29,37,39)]">
           {jokers.map((joker, index) => (
-            <Joker key={index} index={index} joker={joker} />
+            <div draggable
+              onDragStart={() => dragJoker.current = index}
+              onDragEnter={() => draggedOverJoker.current = index}
+              onDragOver={(e) => e.preventDefault()}
+              onDragEnd={handleSort}
+            >
+              <Tilt tiltReverse={true} tiltMaxAngleX={15} tiltMaxAngleY={15}>
+                <div className={`card card-delay-${(index % 4) + 1}`}>
+                  <img src={`/images/jokers/${joker.name}.webp`} alt={joker.name} />
+                </div>
+              </Tilt>
+            </div>
           ))}
         </div>
       </section>
--- src/components/Joker.tsx
+++ src/components/Joker.tsx
@@ -13,35 +13,12 @@ export interface JokerProps {
 
 export const Joker: FC<JokerProps> = ({ index, joker }) => {
   return (
-    <Tilt tiltReverse={true} tiltMaxAngleX={15} tiltMaxAngleY={15}>
-      <div className={`card card-delay-${(index % 4) + 1}`}>
-        <img src={`/images/jokers/${joker}`} alt={joker} />
-      </div>
-    </Tilt>
+    <div draggable>
+      <Tilt tiltReverse={true} tiltMaxAngleX={15} tiltMaxAngleY={15}>
+        <div className={`card card-delay-${(index % 4) + 1}`}>
+          <img src={`/images/jokers/${joker}`} alt={joker} />
+        </div>
+      </Tilt>
+    </div>
   );
 };
-
-interface SortableJokerProps extends SortableElementProps {
-  joker: string;
-  index: number;
-}
-
-export const SortableJoker = SortableElement<SortableJokerProps>(
-  ({ joker, index }) => <Joker index={index} joker={joker} />
-);
-
-interface SortableJokerGridProps {
-  jokers: string[];
-}
-
-export const SortableJokerGrid = SortableContainer<SortableJokerGridProps>(
-  ({ jokers }) => {
-    return (
-      <div className="wrapper grid grid-cols-5 pb-2 -mx-2 gap-2 rounded-xl bg-[rgb(29,37,39)]">
-        {jokers.map((joker, index) => (
-          <SortableJoker key={`joker-${index}`} index={index} joker={joker} />
-        ))}
-      </div>
-    );
-  }
-);
